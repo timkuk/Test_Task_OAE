b20f4569f9429a56da5aa917740c30055bc8c8c6
Tim Fedorenko <tim_fedorenko@mail.ru>
2022-09-26 14:27:36 +0300

    [Test_Task] Result

---
+++ Test_Task_OAE/JsonWorker.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+
+namespace Test_Task_OAE
+{
+    public class JsonWorker
+    {
+        public string GetStringValue(string pathJson, string path)
+        {
+            var valueFromFile = File.ReadAllText(path);
+            var j = JObject.Parse(valueFromFile);
+            var token = j.SelectToken(pathJson);
+
+            if(token == null)
+            {
+                throw new NullReferenceException(); 
+            }
+
+            return token.ToString();
+        }
+    }
+}
---
+++ Test_Task_OAE/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+//1.JSON text from file on disk - without any defined structure, so we do not know what is inside.
+//2. Path to JSON element that tells you 'which element from JSON to take' - path format you can decide on 
+//Your app should search for JSON element via JSON path, convert element to string and print in console app as output
+
+namespace Test_Task_OAE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var path = Path.Combine(Path.GetFullPath(@"test.json"));
+                var jsonWorker = new JsonWorker();
+                Console.WriteLine("Input JSON Path");
+                //car.type[0].sedan.make for example
+                var input = Console.ReadLine();
+                var result = jsonWorker.GetStringValue(input, path);
+                Console.WriteLine(result);
+            }
+            catch
+            {
+                Console.WriteLine("Input invalid JSON path");
+            }     
+        }
+    }
+}
